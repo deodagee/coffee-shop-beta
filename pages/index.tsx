7c34d8ebe6c9d3554dfb7fe63948fda87e130ce3
deodagee <deos_singiza@hotmail.com>
2023-10-16 11:06:08 -0400

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 import Header from '../components/Header';
 import { useSession } from 'next-auth/react';
+import styles from "../styles/main.module.css";
+import Link from 'next/link';
 
 
 export default function Home() {
@@ -24,74 +26,103 @@ export default function Home() {
 
     console.log("User not logged in", session);
 
-  return (
-    <>
-      <Header scrollHandler={scrollHandler} />
-      <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <p
-              className="mt-1 text-3xl font-bold uppercase text-gray-900 sm:text-4xl sm:tracking-tight lg:text-4xl"
-              ref={coffeeRef}
-            >
-              Package Delivery
-            </p>
-            <p
-              className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl"
-              ref={coffeeRef}
-            >
-              24/7
-            </p>
-            <p
-              className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-3xl sm:tracking-tight lg:text-3xl"
-              ref={coffeeRef}
-            >
-              Day/Night
-            </p>
+    return (
+      <>
+      <div className={styles.whole}>
+        <Header scrollHandler={scrollHandler} />
+        <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+            <div className="text-center">
+              <p
+                className="mt-1 text-3xl font-bold uppercase text-gray-900 sm:text-4xl sm:tracking-tight lg:text-4xl"
+                ref={coffeeRef}
+              >
+                Package Delivery
+              </p>
+              <p
+                className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl"
+                ref={coffeeRef}
+              >
+                24/7
+              </p>
+              <p
+                className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-3xl sm:tracking-tight lg:text-3xl"
+                ref={coffeeRef}
+              >
+                Day/Night
+              </p>
+            </div>
+            <div className={styles.bottom_tabs}>
+              <span className={styles.tabs_wrapper}>
+                <Link className={styles.link_piece} href={'/menu'}>
+                  <div className={styles.tabs}><p>Menu</p></div>
+                </Link>
+                <Link className={styles.link_piece} href={'/takeout'}>
+
+                  <div className={styles.tabs}><p>Take Out</p></div>
+                </Link>
+
+              </span>
+            </div>
           </div>
+
         </div>
-
-      </div>
-    </>
-  );
-}
-
-
-if (session) {
-  console.log("User is logged in");
+        </div>
+      </>
+    );
+  }
 
 
-  return (
-    <>
-      <Header scrollHandler={scrollHandler} />
-      <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <p
-              className="mt-1 text-3xl font-bold uppercase text-gray-900 sm:text-4xl sm:tracking-tight lg:text-4xl"
-              ref={coffeeRef}
-            >
-              Package Delivery
-            </p>
-            <p
-              className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl"
-              ref={coffeeRef}
-            >
-              24/7
-            </p>
-            <p
-              className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-3xl sm:tracking-tight lg:text-3xl"
-              ref={coffeeRef}
-            >
-              Day/Night
-            </p>
+  if (session) {
+    console.log("User is logged in");
+
+
+    return (
+      <>
+            <div className={styles.whole}>
+
+        <Header scrollHandler={scrollHandler} />
+        <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="sm:py-15 mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+            <div className="text-center">
+              <p
+                className="mt-1 text-3xl font-bold uppercase text-gray-900 sm:text-4xl sm:tracking-tight lg:text-4xl"
+                ref={coffeeRef}
+              >
+                Package Delivery
+              </p>
+              <p
+                className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-5xl"
+                ref={coffeeRef}
+              >
+                24/7
+              </p>
+              <p
+                className="mt-1 text-4xl font-bold uppercase text-gray-900 sm:text-3xl sm:tracking-tight lg:text-3xl"
+                ref={coffeeRef}
+              >
+                Day/Night
+              </p>
+            </div>
+            <div className={styles.bottom_tabs}>
+              <span className={styles.tabs_wrapper}>
+                <Link className={styles.link_piece} href={'/menu'}>
+                  <div className={styles.tabs}><p>Menu</p></div>
+                </Link>
+                <Link className={styles.link_piece} href={'/takeout'}>
+
+                  <div className={styles.tabs}><p>Take Out</p></div>
+                </Link>
+
+              </span>
+            </div>
           </div>
-        </div>
+          </div>
 
-      </div>
-    </>
-  );
-}
+        </div>
+      </>
+    );
+  }
 }
 
 

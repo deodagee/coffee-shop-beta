6c0d6ef3aa94eb8fe933e1540212663e9a81243a
deodagee <deos_singiza@hotmail.com>
2023-10-16 13:41:58 -0400

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,22 @@
 import Link from "next/link";
+import styles from '../styles/footer.module.css';
+
 
 export default function Footer() {
   return (
-    <footer className="font-sans font-bold center mt-5 flex justify-center space-x-1 bg-[#E7E8EF] p-4 text-xs">
-      <img  src="/logowhitesmall.png" alt="Logo" width={5} height={5} className=" translate-y-[-2px] mr-[8px] h-5 w-5" /> {/* Replace "/logowhitesmall.png" with the correct path to your image */}
-      <p>Designed By</p>
-      <span className="text-zinc-500">Deo Siniza</span>
-      <span>|</span>
-      <a href="https://astrumstellar.com" className="font-sans font-medium text-orange-600">
+    <footer className={styles.footer_wrapper}>
+      <img  src="/logowhitesmall.png" alt="Logo" width={5} height={5} className={styles.footer_logo} /> {/* Replace "/logowhitesmall.png" with the correct path to your image */}
+      <Link
+      href={"https://astrumstellar.com"}>
+        <span className={styles.name_credentials}>
+      <p className={styles.designed_by}>Designed By</p>
+      <p className={styles.name} >Deo Siniza</p>
+      </span>
+      </Link>
+      <span className={styles.span_item}>|</span>
+      <Link href="https://github.com/deodagee/coffee-shop-beta" className={styles.github_link}>
         Github Code
-      </a>
+      </Link>
     </footer>
   );
 }
